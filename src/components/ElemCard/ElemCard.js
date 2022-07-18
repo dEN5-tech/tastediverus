90c73a9578c7d65fbab218adba13e475533ad8f6
dEN5 <moyadres_1@mail.ru>
2022-07-18 13:59:45 +0300

    fix link in frontend

--- src/components/ElemCard/ElemCard.js
+++ src/components/ElemCard/ElemCard.js
@@ -48,7 +48,9 @@ const ElemCard = ({ title, srcset, id, likes, rating, year, width, history, data
 
 
     async function GetAll(q, type) {
-        const response = await axios.get(`https://tastediverus.herokuapp.com/api/SearchAll`, {
+
+        if(["m","t"].includes(type)){
+            const response = await axios.get(`https://tastediverus.herokuapp.com/api/search_show_movie`, {
             params: {
                 'query': q,
                 'type': type
@@ -71,6 +73,32 @@ const ElemCard = ({ title, srcset, id, likes, rating, year, width, history, data
             }
         })
         return response.data
+    }else if(["s"].includes(type)){
+        const response = await axios.get(`https://tastediverus.herokuapp.com/api/search_music`, {
+            params: {
+                'query': q,
+                'type': type
+            },
+            headers: {
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6,zh;q=0.5',
+                'Cache-Control': 'max-age=0',
+                'Connection': 'keep-alive',
+                'If-None-Match': 'W/"18ba-xlcu4SzE3rzJSp/5L5XbCfbiXIQ"',
+                'Sec-Fetch-Dest': 'document',
+                'Sec-Fetch-Mode': 'navigate',
+                'Sec-Fetch-Site': 'none',
+                'Sec-Fetch-User': '?1',
+                'Upgrade-Insecure-Requests': '1',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
+                'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"'
+            }
+        })
+        return response.data
+    }
+        
     }
 
     function IndientElem(type_, data) {
