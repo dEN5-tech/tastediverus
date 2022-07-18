70703472601004da4184f3967bbf7d1ab86bc386
dEN5 <moyadres_1@mail.ru>
2022-07-18 23:01:24 +0300

    fix api

--- servere/main.js
+++ servere/main.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 var SpotifyWebApi = require('spotify-web-api-node');
 const { YMApi } = require("ym-api");
 const api = new YMApi();
-
+const JP = require('jsonpath') ;
 
 
 const puppeteer = require('puppeteer');
@@ -136,7 +136,7 @@ async function kinopoisk_query(query) {
         }
     }).then(()=>{return response.data.films[0]})
     .catch(err=>{return {data: null}})
-    
+
 }
 
 
@@ -178,13 +178,84 @@ async function myshows_query(query, id) {
 
 
 
+async function getQueryKino(elem){
+    const dt = await axios.post(
+        'https://graphql.kinopoisk.ru/graphql/', {
+            'operationName': 'SuggestSearch',
+            'variables': {
+                'keyword': `${elem.title} ${elem.year}`,
+                'yandexCityId': 5,
+                'limit': 1
+            },
+            'query': 'query SuggestSearch($keyword: String!, $yandexCityId: Int, $limit: Int) { suggest(keyword: $keyword) { top(yandexCityId: $yandexCityId, limit: $limit) { topResult { global { ...SuggestMovieItem ...SuggestPersonItem ...SuggestCinemaItem ...SuggestMovieListItem __typename } __typename } movies { movie { ...SuggestMovieItem __typename } __typename } persons { person { ...SuggestPersonItem __typename } __typename } cinemas { cinema { ...SuggestCinemaItem __typename } __typename } movieLists { movieList { ...SuggestMovieListItem __typename } __typename } __typename } __typename } } fragment SuggestMovieItem on Movie { id title { russian original __typename } rating { kinopoisk { isActive value __typename } __typename } poster { avatarsUrl fallbackUrl __typename } viewOption { buttonText isAvailableOnline: isWatchable(filter: {anyDevice: false, anyRegion: false}) purchasabilityStatus subscriptionPurchaseTag type availabilityAnnounce { groupPeriodType announcePromise availabilityDate type __typename } __typename } ... on Film { type productionYear __typename } ... on TvSeries { releaseYears { end start __typename } __typename } ... on TvShow { releaseYears { end start __typename } __typename } ... on MiniSeries { releaseYears { end start __typename } __typename } __typename } fragment SuggestPersonItem on Person { id name originalName birthDate poster { avatarsUrl fallbackUrl __typename } __typename } fragment SuggestCinemaItem on Cinema { id ctitle: title city { id name geoId __typename } __typename } fragment SuggestMovieListItem on MovieListMeta { id cover { avatarsUrl __typename } coverBackground { avatarsUrl __typename } name url description movies(limit: 0) { total __typename } __typename } '
+        }, {
+            params: {
+                'operationName': 'SuggestSearch'
+            },
+            headers: {
+                'authority': 'graphql.kinopoisk.ru',
+                'accept': '*/*',
+                'accept-language': 'ru,en;q=0.9',
+                'cache-control': 'no-cache',
+                'content-type': 'application/json',
+                'cookie': 'yandexuid=1656658121627748886; yandex_login=twist.mas; mda_exp_enabled=1; i=gTlVadgMRXNT60jI0ekuxf9cw63dCD0HtgdeMuVR4OpZSi7GOvGYYmC4W6EGnfkEuH5rnq44sb2oRH91GilE46ukQCw=; ya_sess_id=3:1629316637.5.0.1628413571717:hi1tWQ:2a.1|888287499.-1.2.1:189654856|30:200744.287915.2bCVasFJwIM8uhErkxP3FYsCtH0; mda2_beacon=1629316637654; _yasc=M8tv0Ax3tFkhPdu1fY/2MSZfe7xCmsvebsXjnBZ5EqWnXg==; location=1; crookie=WiNL182ya+RL4l6PbkmgV720cnjxCgyWxExYSxbOIEjmMxcOAgFtK9Jf/U1GMbuYFnOaiYoLbkngruMydklZ9X6fC0c=; cmtchd=MTY1ODE2MjE2OTExNA==; sso_status=sso.passport.yandex.ru:blocked; PHPSESSID=b0d8d22371d5d5ed7f181f01f6bffe08; user_country=ru; yandex_gid=47; tc=5',
+                'origin': 'https://www.kinopoisk.ru',
+                'pragma': 'no-cache',
+                'referer': 'https://www.kinopoisk.ru/',
+                'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'same-site',
+                'service-id': '25',
+                'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+                'x-kp-searchexps': 'experiment3',
+                'x-kp-testids': '604146,605606,614193,436979,573975,598964,606836,600434,606888',
+                'x-request-id': '1658166051888870-5681630445875880107',
+                'x-search-request-id': `${Date.now()}`
+            }
+        }
+    )
+    return JP.query(dt.data.data,`$.*.*.topResult.global`).pop()
+}
 
 
 
-app.get('/get_data', async (req, res) => {
-    let type_data = req.query.type == "1" ?  "recommended" : `recommended/qt-${req.query.type}`
 
-    axios.get(`https://tastedive.com/fragment/${type_data||"recommended"}/start-${req.query.offset||"0"}/rpp-${req.query.count||"12"}`, {
+async function callerdt(items) {
+    let ie = [];
+                for(let elem in items){
+                    let r = await getQueryKino(items[elem])
+                    if(r === undefined){}else if(items[elem].type==="shows"||"movies" && r.__typename!="Person"){
+                        console.log(r)
+                        ie.push({
+                            ...items[elem],
+                            title: !!r.title.russian ? r.title.russian : items[elem].title,
+                            kinopoisk_id: r.id
+                        })
+                    } else {
+                        ie.push({
+                            ...items[elem]
+                        })
+                    }
+
+                    }
+
+
+                    
+              console.log(ie)
+               
+
+                return ie
+}
+
+
+
+app.get('/get_data', async(req, res) => {
+    let type_data = req.query.type == "1" ? "recommended" : `recommended/qt-${req.query.type}`
+
+    axios.get(`https://tastedive.com/fragment/${type_data || "recommended"}/start-${req.query.offset || "0"}/rpp-${req.query.count || "12"}`, {
         headers: {
             'authority': 'tastedive.com',
             'accept': '*/*',
@@ -201,15 +272,17 @@ app.get('/get_data', async (req, res) => {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36'
         }
     }).then(r => {
-        if (r.data == undefined){
-            res.json({data: null})
-        }else{
-            var promise = htmlToJson.parse(r.data.toString(), function () {
-                let num =  1
+        if (r.data == undefined) {
+            res.json({
+                data: null
+            })
+        } else {
+            htmlToJson.parse(r.data.toString(), function () {
+                let num = 1
                 return this.map('div[class*="entity entity-card grid-item js-entity"]', function ($item) {
 
                     var dtn = {
-                        title:$item.find('span.entity-title').text(),
+                        title: $item.find('span.entity-title').text(),
                         srcset: $item.find('div.entity-image-wrap > img').attr("srcset"),
                         id: $item.find('div.entity-card-content > a').attr("id"),
                         likes: $item.find('div.entity-opine > button.opine.like.js-opine-ex > span.count').text(),
@@ -225,18 +298,18 @@ app.get('/get_data', async (req, res) => {
                 });
             }).done(function (items) {
 
-                        res.json({data: items})
+                callerdt(items).then((ie)=>res.json({
+                    data: ie
+                }))
 
-            }, function (err) {
-                res.json({data: null})
-            });
+                
+            })
         }
 
-    }
-    ).catch(()=>{
-        res.json({data: null})
     })
 })
+
+
 
 
 app.get('/get_data_sim', async (req, res) => {
